40b4cdb4a25e315d0a62bbcf1fa1ec94a30934ad
ULlllDrar <87375360+ULlllDrar@users.noreply.github.com>
2022-08-15 17:25:39 +0900

    コメントを削除
    Co-authored-by: yamaji <haruyuki.program@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
-//実行ページ：http://localhost:8000/today
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
