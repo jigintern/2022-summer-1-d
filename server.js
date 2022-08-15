f48de5c46a54aa1c2877da7b243dc69ddd20f910
ULlllDrar <123tellur@gmail.com>
2022-08-15 17:07:28 +0900

    変更ヤユヨ

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
+//実行ページ：http://localhost:8000/today
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
@@ -11,6 +12,10 @@ serve(async (req) => {
 
   if (req.method === "GET" && pathname === "/members") {
     return new Response("ウノ、ひより、ヤユヨ、やまじ");
+  }
+  if (req.method === "GET" && pathname === "/today") {
+    const date = new Date();
+    return new Response(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
   }
 
   return serveDir(req, {
