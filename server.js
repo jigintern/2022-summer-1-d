7d3c5ee839e694b10063815405b41e4ca8587f02
ULlllDrar <87375360+ULlllDrar@users.noreply.github.com>
2022-08-15 17:27:51 +0900

    コメントを追加
    Co-authored-by: yamaji <haruyuki.program@gmail.com>

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ serve(async (req) => {
   if (req.method === "GET" && pathname === "/members") {
     return new Response("ウノ、ひより、ヤユヨ、やまじ");
   }
+<<<<<<< HEAD
+=======
+  if (req.method === "GET" && pathname === "/today") {
+    const date = new Date();
+    // date.getMonth()で帰ってくる月の数字は0~11月なので+1して1~12月になるようにする
+    return new Response(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+  }
+>>>>>>> 3f1e7ad (コメントを追加)
 
   return serveDir(req, {
     fsRoot: "public",
