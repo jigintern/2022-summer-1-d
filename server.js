1f180172c4f075c7c8c8da416befd1d48fd2b8ba
DESKTOP-NRBEP2J\yuzu7 <m19-bjig@kure.kosen-ac.jp>
2022-08-19 12:55:08 +0900

    commit

--- server.js
+++ server.js
@@ -3,104 +3,104 @@ import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 
-// // Get the connection string from the environment variable "DATABASE_URL"
-// const databaseUrl = Deno.env.get("DATABASE_URL");
+ // Get the connection string from the environment variable "DATABASE_URL"
+ const databaseUrl = Deno.env.get("DATABASE_URL");
 
-// // Create a database pool with three connections that are lazily established
-// const pool = new postgres.Pool(databaseUrl, 3, true);
+ // Create a database pool with three connections that are lazily established
+ const pool = new postgres.Pool(databaseUrl, 3, true);
 
-// // Connect to the database
-// const connection = await pool.connect();
-// try {
-//   // Create the table
-//   await connection.queryObject`
-//     CREATE TABLE IF NOT EXISTS status (
-//       id SERIAL PRIMARY KEY,
-//       hp INTEGER NOT NULL,
-//       atk INTEGER NOT NULL
-//     )
-//   `;
-// } finally {
-//   // Release the connection back into the pool
-//   connection.release();
-// }
+ // Connect to the database
+ const connection = await pool.connect();
+ try {
+   // Create the table
+   await connection.queryObject`
+     CREATE TABLE IF NOT EXISTS status (
+       id SERIAL PRIMARY KEY,
+       hp INTEGER NOT NULL,
+       atk INTEGER NOT NULL
+     )
+   `;
+ } finally {
+   // Release the connection back into the pool
+   connection.release();
+ }
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-// if (req.method === "GET" && pathname === "/welcome-message") {
-//     return new Response("jigインターンへようこそ！");
-//   }
+ if (req.method === "GET" && pathname === "/welcome-message") {
+     return new Response("jigインターンへようこそ！");
+   }
 
-//   if (req.method === "GET" && pathname === "/members") {
-//     return new Response("ウノ、ひより、ヤユヨ、やまじ");
-//   }
-//   if (req.method === "GET" && pathname === "/today") {
-//     const date = new Date();
-//     // date.getMonth()で帰ってくる月の数字は0~11月なので+1して1~12月になるようにする
-//     return new Response(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
-//   }
+   if (req.method === "GET" && pathname === "/members") {
+     return new Response("ウノ、ひより、ヤユヨ、やまじ");
+   }
+   if (req.method === "GET" && pathname === "/today") {
+     const date = new Date();
+     // date.getMonth()で帰ってくる月の数字は0~11月なので+1して1~12月になるようにする
+     return new Response(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+   }
 
-//   if (pathname == "/player") {
-//     // Grab a connection from the database pool
-//     const connection = await pool.connect();
-//     console.log("fire")
-//     try {
-//       switch (req.method) {
-//         case "GET": { // This is a GET request. Return a list of all todos.
-//           // Run the query
-//           const params = new URLSearchParams(req.url.substring(req.url.indexOf("?")));
-//           const ID = params.get("id");
-//           console.log(`id: ${ID}`);
+   if (pathname == "/player") {
+     // Grab a connection from the database pool
+     const connection = await pool.connect();
+     console.log("fire")
+     try {
+       switch (req.method) {
+         case "GET": { // This is a GET request. Return a list of all todos.
+           // Run the query
+           const params = new URLSearchParams(req.url.substring(req.url.indexOf("?")));
+           const ID = params.get("id");
+           console.log(`id: ${ID}`);
 
-//           const result = await connection.queryObject`
-//             SELECT * FROM status WHERE id=${ID}
-//           `;
+           const result = await connection.queryObject`
+             SELECT * FROM status WHERE id=${ID}
+           `;
 
-//           // Encode the result as JSON
-//           const body = JSON.stringify(result.rows, null, 2);
+           // Encode the result as JSON
+           const body = JSON.stringify(result.rows, null, 2);
 
-//           // Return the result as JSON
-//           return new Response(body, {
-//             headers: { "content-type": "application/json" },
-//           });
-//         }
-//         case "POST": { // This is a POST request. Create a new todo.
-//           // Parse the request body as JSON. If the request body fails to parse,
-//           // is not a string, or is longer than 256 chars, return a 400 response.
-//           const params = new URLSearchParams(req.url.substring(req.url.indexOf("?")));
-//           const ID = params.get("id");
-//           const status = await req.json().catch(()=>null);
-//           console.log(`id: ${ID}\nstatus: ${status} (type: ${typeof status})`);
-//           if (typeof status !== "object") {
-//             console.log("Bad Reqest");
-//             return new Response("Bad Request", { status: 400 });
-//           }
+           // Return the result as JSON
+           return new Response(body, {
+             headers: { "content-type": "application/json" },
+           });
+         }
+         case "POST": { // This is a POST request. Create a new todo.
+           // Parse the request body as JSON. If the request body fails to parse,
+           // is not a string, or is longer than 256 chars, return a 400 response.
+           const params = new URLSearchParams(req.url.substring(req.url.indexOf("?")));
+           const ID = params.get("id");
+           const status = await req.json().catch(()=>null);
+           console.log(`id: ${ID}\nstatus: ${status} (type: ${typeof status})`);
+           if (typeof status !== "object") {
+             console.log("Bad Reqest");
+             return new Response("Bad Request", { status: 400 });
+           }
 
-//           // Insert the new todo into the database
-//           await connection.queryObject`
-//             UPDATE status set hp=${status.hp}, atk=${status.atk} WHERE id=${ID}
-//           `;
+           // Insert the new todo into the database
+           await connection.queryObject`
+             UPDATE status set hp=${status.hp}, atk=${status.atk} WHERE id=${ID}
+           `;
 
-//           // Return a 201 Created response
-//           return new Response("", { status: 201 });
-//         }
-//         default: // If this is neither a POST, or a GET return a 405 response.
-//           return new Response("Method Not Allowed", { status: 405 });
-//       }
-//     }
-//     catch (err) {
-//       console.error(err);
-//       // If an error occurs, return a 500 response
-//       return new Response(`Internal Server Error\n\n${err.message}`, {
-//         status: 500,
-//       });
-//     } finally {
-//       // Release the connection back into the pool
-//       connection.release();
-//     }
-//   }
+           // Return a 201 Created response
+           return new Response("", { status: 201 });
+         }
+         default: // If this is neither a POST, or a GET return a 405 response.
+           return new Response("Method Not Allowed", { status: 405 });
+       }
+     }
+     catch (err) {
+       console.error(err);
+       // If an error occurs, return a 500 response
+       return new Response(`Internal Server Error\n\n${err.message}`, {
+         status: 500,
+       });
+     } finally {
+       // Release the connection back into the pool
+       connection.release();
+     }
+   }
 
   return serveDir(req, {
     fsRoot: "public",
