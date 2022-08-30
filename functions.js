436a8cb34c58cfd55ef90fc05104ba2adcbb411f
ULlllDrar <123tellur@gmail.com>
2022-08-30 14:22:13 +0900

    👍 使いやすくした

--- functions.js
+++ functions.js
@@ -2,16 +2,14 @@ export const accessToAPI={
   async temperture(latitude,longitude){
     return await fetch(`/temp?latitude=${latitude}&longitude=${longitude}`)
   },
-  player:{
-    async get(location_href){
-      return (await(await fetch(location_href)).json())[0]
-    },
-    async post(location_href,hp,atk){
-      await fetch(location_href, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ hp,atk })
-      });
-    }
+  async playerData(){
+      return (await(await fetch(location.href)).json())[0]
+  },
+  async save(hp,atk){
+    await fetch(location.href, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ hp,atk })
+    });
   }
 }
